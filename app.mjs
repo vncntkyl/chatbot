919d77c9f62cc84e957658f8109da115675f2f02
Vncntkyl <kyle.rinoza2009@gmail.com>
2024-03-19 15:18:28 +0800

    build: install token and add webhook request

--- app.mjs
+++ app.mjs
@@ -1,10 +1,29 @@
 import express from "express";
+import dotenv from dotenv;
+
 const app = express();
 
 const port = 3000;
+dotenv.config()
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+app.get("/webhook", (req, res) => {
+  // Parse the query params
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Check if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Check the mode and token sent is correct
+    if (mode === "subscribe" && token === process.env.TOKEN) {
+      // Respond with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Respond with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
 });
 
 app.listen(port, () => {
